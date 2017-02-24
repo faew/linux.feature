33ec952fb6dc98902f38b9e3d92e58cf44e8f1fa
Vadim Zimin <faew@rambler.ru>
2017-02-24 19:22:12 +0000

    Fix Fail2Ban selinux

--- script/centos7/04-install-fail2ban.sh
+++ script/centos7/04-install-fail2ban.sh
@@ -4,5 +4,8 @@
 yum install fail2ban
 echo "[sshd]" >  /etc/fail2ban/jail.d/customisation.local
 echo "enabled = true" >>  /etc/fail2ban/jail.d/customisation.local
+mkdir /var/run/fail2ban
+chmod 644 /var/run/fail2ban
+restorecon -v /var/run/fail2ban
 systemctl start fail2ban
 systemctl enable fail2ban
