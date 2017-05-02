d7cfc430011b7cd3e9fb46f7c5789d003b5fba04
faew <faew@rambler.ru>
2017-05-02 22:33:46 +0400

    Add enable auditd

--- script/centos7/00-update-system.sh
+++ script/centos7/00-update-system.sh
@@ -3,5 +3,10 @@
 
 yum -y update
 yum -y install selinux-policy-targeted
+
+yum -y install audit audit-libs
+systemctl enable auditd
+systemctl start auditd
+
 sed -i "s/SELINUX=disabled/SELINUX=enforcing/g" /etc/selinux/config
 reboot
