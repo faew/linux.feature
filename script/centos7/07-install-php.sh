bd7356fcf0433dabfe7ffc97ee244a2765e74d73
faew <faew@rambler.ru>
2017-06-11 11:20:50 +0400

    add php-process

--- script/centos7/07-install-php.sh
+++ script/centos7/07-install-php.sh
@@ -1,7 +1,7 @@
 # Copyright © 2017 Feature.su. All rights reserved.
 # Licensed under the Apache License, Version 2.0
 
-yum -y install php php-pdo php-mysql php-pear php-devel php-soap php-xml php-gd php-mcrypt php-dba php-mbstring php-pgsql
+yum -y install php php-pdo php-mysql php-pear php-devel php-soap php-xml php-gd php-mcrypt php-dba php-mbstring php-pgsql php-posix
 
 sed -i "s/short_open_tag = Off/short_open_tag = On/g" /etc/php.ini
 sed -i "s/expose_php = On/expose_php = Off/g" /etc/php.ini
