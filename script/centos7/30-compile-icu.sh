8ba2f29b706a123ce391cb2c08b2e7b084a0c824
faew <faew@ru5.net>
2017-10-24 20:40:25 +0400

    icu compile without php-intl

---
+++ script/centos7/30-compile-icu.sh
@@ -0,0 +1,10 @@
+# Copyright © 2017 Feature.su. All rights reserved.
+# Licensed under the Apache License, Version 2.0 
+
+cd /tmp
+wget http://download.icu-project.org/files/icu4c/58.2/icu4c-58_2-src.tgz
+tar -xvzf ./icu4c-58_2-src.tgz
+cd /tmp/icu/source
+./configure
+make
+make install
