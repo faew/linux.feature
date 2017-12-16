e87dee990fce0f6a9763c2168120b5b64dd91eb3
faew <faew@ru5.net>
2017-12-16 14:39:24 +0400

    install crontabs

---
+++ script/centos7/35-install-crond.sh
@@ -0,0 +1,11 @@
+# Copyright © 2017 Feature.su. All rights reserved. 
+# Licensed under the Apache License, Version 2.0 
+
+if [ "`systemctl is-active crond`" != "active" ]  
+then 
+yum -y install cronie
+systemctl start crond
+systemctl enable crond
+else
+echo "Warning: crond already install"
+fi
