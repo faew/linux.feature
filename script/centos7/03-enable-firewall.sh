567a7272dbed92bbcda03e060440c9c04e962514
faew <faew@ru5.net>
2017-12-16 11:25:23 +0400

    install rpm firewalld

--- script/centos7/03-enable-firewall.sh
+++ script/centos7/03-enable-firewall.sh
@@ -1,6 +1,7 @@
 # Copyright © 2017 Feature.su. All rights reserved.
 # Licensed under the Apache License, Version 2.0
 
+yum -y install firewalld
 systemctl start firewalld
 firewall-cmd --zone=public --change-interface=`basename -a /sys/class/net/* | grep -v lo | tail -n 1`
 firewall-cmd --reload
