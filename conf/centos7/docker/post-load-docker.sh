22a292d9182fb0b3b9519a49de4bff84d052c628
faew <faew@ru5.net>
2017-12-17 21:54:15 +0400

    delete firewall inner docker

--- conf/centos7/docker/post-load-docker.sh
+++
@@ -1,8 +0,0 @@
-# Copyright © 2017 Feature.su. All rights reserved.
-# Licensed under the Apache License, Version 2.0 
-
-sh /root/linux.feature/script/centos7/03-enable-firewall.sh
-systemctl restart fail2ban
-sh /root/linux.feature/script/centos7/08.2-set-pass-mysql.sh `pwgen -y 12 1`
-firewall-cmd --zone=public --permanent --add-service=http
-firewall-cmd --reload
