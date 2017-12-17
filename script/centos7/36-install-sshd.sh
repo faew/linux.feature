1b71668f0e8ff75ab2bce7ac9bbc2deed93b8c97
faew <faew@ru5.net>
2017-12-17 16:41:30 +0400

    fix space

--- script/centos7/36-install-sshd.sh
+++ script/centos7/36-install-sshd.sh
@@ -1,11 +1,11 @@
 # Copyright © 2017 Feature.su. All rights reserved.
 # Licensed under the Apache License, Version 2.0
 
-if [ "`systemctl is-active sshd`" != "active" ]   
-then  
+if [ "`systemctl is-active sshd`" != "active" ]
+then
 yum -y install openssh-server
 systemctl start sshd
 systemctl enable sshd
-else 
+else
 echo "Warning: sshd already install" 
-fi 
+fi
