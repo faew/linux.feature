5a5af7a2a2999286f228f762c3a06e4552b0d504
faew <faew@ru5.net>
2017-12-17 16:42:10 +0400

    fix space

--- script/centos7/35-install-crond.sh
+++ script/centos7/35-install-crond.sh
@@ -1,8 +1,8 @@
 # Copyright © 2017 Feature.su. All rights reserved. 
 # Licensed under the Apache License, Version 2.0 
 
-if [ "`systemctl is-active crond`" != "active" ]  
-then 
+if [ "`systemctl is-active crond`" != "active" ]
+then
 yum -y install cronie
 systemctl start crond
 systemctl enable crond
