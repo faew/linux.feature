70cbb77cbbe596fc137fe334109381268ea1397f
faew <faew@ru5.net>
2017-12-31 18:46:35 +0400

    mariadb not correct GROUP BY command

--- script/centos7/08.1-install-mariadb.sh
+++
@@ -1,8 +0,0 @@
-# Copyright © 2017 Feature.su. All rights reserved.
-# Licensed under the Apache License, Version 2.0
-
-yum install mariadb-server
-systemctl enable mariadb
-systemctl start mariadb
-mysql_secure_installation
-
