b141a59237f972085b9676ff79cc22356dc34358
faew <faew@ru5.net>
2018-01-05 08:48:56 +0000

    Update repo

--- script/centos7/00.2-install-docker.sh
+++ script/centos7/00.2-install-docker.sh
@@ -3,7 +3,9 @@
 
 if [ "`systemctl is-active docker`" != "active" ]
 then
-curl -fsSL https://get.docker.com/ | sh
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
 systemctl start docker
 systemctl enable docker
 reboot
