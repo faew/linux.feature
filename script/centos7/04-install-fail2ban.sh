a757fb571ea9201ab576b1e5111cb96110e960f9
faew <faew@ru5.net>
2018-01-21 14:05:25 +0000

    No history database, big and not optimal

--- script/centos7/04-install-fail2ban.sh
+++ script/centos7/04-install-fail2ban.sh
@@ -9,6 +9,7 @@ echo "enabled = true" >>  /etc/fail2ban/jail.d/customisation.local
 mkdir /var/run/fail2ban
 chmod 644 /var/run/fail2ban
 restorecon -v /var/run/fail2ban
+sed -i "s/= \/var\/lib\/fail2ban\/fail2ban.sqlite3/= None/g" /etc/fail2ban/fail2ban.conf
 systemctl start fail2ban
 systemctl enable fail2ban
 else
