d79ddeb07913be5e69b8b6b94e74596709f9c89d
faew <faew@ru5.net>
2018-01-21 14:52:53 +0000

    Disable IPv6

--- script/centos7/05.1-install-apache-ssl.sh
+++ script/centos7/05.1-install-apache-ssl.sh
@@ -2,5 +2,6 @@
 # Licensed under the Apache License, Version 2.0
 
 yum -y install mod_ssl
+sed -i "s/Listen 443 https/Listen 0.0.0.0:443/g" /etc/httpd/conf.d/ssl.conf
 firewall-cmd --zone=public --permanent --add-service=https
 firewall-cmd --reload
