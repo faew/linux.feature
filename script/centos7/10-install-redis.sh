b5240dee67563476d1a016c2c551337ece81e3cb
faew <faew@ru5.net>
2018-02-01 18:42:49 +0000

    Add socket

--- script/centos7/10-install-redis.sh
+++ script/centos7/10-install-redis.sh
@@ -2,6 +2,10 @@
 # Licensed under the Apache License, Version 2.0
 
 yum -y install redis
+
+sed -i "s/# unixsocket \/tmp\/redis.sock/unixsocket \/var\/run\/redis\/redis.sock/g" /etc/redis.conf
+sed -i "s/# unixsocketperm 700/unixsocketperm 777/g" /etc/redis.conf
+
 systemctl start redis
 systemctl enable redis
 echo "Configuration: /etc/redis.conf"
