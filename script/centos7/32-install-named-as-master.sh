ab21c13a009ef2943158937ffcb64ac8944de010
faew <faew@ru5.net>
2018-03-15 17:56:08 +0000

    Allow recursion from localhost

--- script/centos7/32-install-named-as-master.sh
+++ script/centos7/32-install-named-as-master.sh
@@ -1,4 +1,4 @@
-# Copyright © 2017 Feature.su. All rights reserved.
+# Copyright © 2018 Feature.su. All rights reserved.
 # Licensed under the Apache License, Version 2.0
 
 if [ "`systemctl is-active named-chroot`" != "active" ]
@@ -9,8 +9,7 @@ systemctl stop named-chroot
 
 sed -i "s/127\.0\.0\.1/any/g" /etc/named.conf
 sed -i "s/::1/none/g" /etc/named.conf
-sed -i "s/localhost;/any;/g" /etc/named.conf
-sed -i "s/recursion yes;/recursion no;/g" /etc/named.conf
+sed -i "s/allow-query/allow-recursion/g" /etc/named.conf
 
 systemctl enable named-chroot
 systemctl start named-chroot
