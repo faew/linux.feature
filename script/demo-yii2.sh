f35fd072d872a383080eb867a25bc5c1fdbb9e81
faew <faew@ru5.net>
2018-04-30 22:32:25 +0000

    Create demo-yii2.sh

---
+++ script/demo-yii2.sh
@@ -0,0 +1,22 @@
+# Copyright © 2018 Feature.su. All rights reserved. 
+# Licensed under the Apache License, Version 2.0  
+
+if [ -z $1  ]
+then
+echo "Usage for demo yii: sh demo-yii2.sh domain.name.without.www"
+else
+ if [ -d /var/www/$1 ]
+ then
+ cd /var/www/$1/html/web
+ wget https://github.com/yiisoft/yii2/releases/download/2.0.15/yii-basic-app-2.0.15.tgz
+ tar -xvzf yii-basic-app-2.0.15.tgz
+ mkdir vendor
+ cp -R /var/www/$1/html/web/basic/vendor/yiisoft /var/www/$1/html/web/vendor
+ cp -R /var/www/$1/html/web/basic/requirements.php /var/www/$1/html/web/requirements.php
+ rm -f -R /var/www/$1/html/web/basic
+ rm -f /var/www/$1/html/web/yii-basic-app-2.0.15.tgz
+ chown -R $1:$1 /var/www/$1/html/web
+ else
+ echo "Domain NOT $1 exists";
+ fi
+fi
