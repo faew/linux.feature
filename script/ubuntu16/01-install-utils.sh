07d5b48f162a3f9524be2d724af850f06fd09068
faew <faew@ru5.net>
2018-04-30 22:23:27 +0000

    Create 01-install-utils.sh

---
+++ script/ubuntu16/01-install-utils.sh
@@ -0,0 +1,10 @@
+# Copyright © 2018 Feature.su. All rights reserved. 
+# Licensed under the Apache License, Version 2.0  
+
+apt-get -y install psmisc
+apt-get -y install mc telnet nano traceroute
+apt-get -y install wget net-tools
+apt-get -y install rdiff-backup rsync sshpass sshfs
+apt-get -y install pwgen
+apt-get -y install supervisor
+apt-get -y install unzip
