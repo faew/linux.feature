e006d422d070b65d9f1185cc9ae36c37962f9065
faew <faew@ru5.net>
2018-05-22 18:35:22 +0000

    add system folders

--- script/ubuntu16/00-update-system.sh
+++ script/ubuntu16/00-update-system.sh
@@ -2,4 +2,13 @@
 # Licensed under the Apache License, Version 2.0  
 
 apt-get -y update
+
+mkdir /etc/linux.feature
+chmod 700 /etc/linux.feature
+
+mkdir /var/backup
+chmod 700 /var/backup
+
+mkdir /iso/
+
 reboot
