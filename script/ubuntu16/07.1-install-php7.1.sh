47c7947b39e1725f9b5c1958caa2f2d01036eaf0
faew <faew@ru5.net>
2018-08-29 16:33:00 +0000

    Add php-zip

--- script/ubuntu16/07.1-install-php7.1.sh
+++ script/ubuntu16/07.1-install-php7.1.sh
@@ -5,7 +5,7 @@ apt-get -y install python-software-properties
 add-apt-repository ppa:ondrej/php
 apt-get update
 
-apt-get -y install php7.1 php7.1-curl php7.1-mysql php7.1-dev php7.1-soap php7.1-xml php7.1-gd php7.1-mcrypt php7.1-dba php7.1-mbstring php7.1-pgsql php7.1-sqlite3 php7.1-memcached php7.1-imagick php7.1-intl
+apt-get -y install php7.1 php7.1-zip php7.1-curl php7.1-mysql php7.1-dev php7.1-soap php7.1-xml php7.1-gd php7.1-mcrypt php7.1-dba php7.1-mbstring php7.1-pgsql php7.1-sqlite3 php7.1-memcached php7.1-imagick php7.1-intl
 
 a2dismod php7.0
 a2enmod php7.1
