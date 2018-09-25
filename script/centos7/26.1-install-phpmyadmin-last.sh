97da31e6ea3544dba3e97c66eb9fa4dd49855783
faew <faew@ru5.net>
2018-09-25 07:28:11 +0000

    Move DocRoot

--- script/centos7/26.1-install-phpmyadmin-last.sh
+++ script/centos7/26.1-install-phpmyadmin-last.sh
@@ -1,11 +1,11 @@
 # Copyright © 2017 Feature.su. All rights reserved.
 # Licensed under the Apache License, Version 2.0
 
-cd /var/www/empty.local/html/web/
-wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-english.tar.gz
-tar -xvzf ./phpMyAdmin-4.7.7-english.tar.gz
-rm -f ./phpMyAdmin-4.7.7-english.tar.gz
-mv phpMyAdmin-4.7.7-english pma-`pwgen -A 12 1`
+cd /var/www/empty.local/project/html/web/
+wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-english.tar.gz
+tar -xvzf ./phpMyAdmin-4.8.3-english.tar.gz
+rm -f ./phpMyAdmin-4.8.3-english.tar.gz
+mv phpMyAdmin-4.8.3-english pma-`pwgen -A 12 1`
 cd pma-*
 rm -f -R setup
 cp config.sample.inc.php config.inc.php
@@ -13,4 +13,4 @@ sed -i "s/blowfish_secret'] = ''/blowfish_secret'] = '`pwgen 64 1`'/g" ./config.
 cd ..
 cd ..
 chown -R empty.local:empty.local ./web
-echo "Open http://MAIN-IP/`ls -1 /var/www/empty.local/html/web/ | grep "pma-" | tail -n 1`"
+echo "Open http://MAIN-IP/`ls -1 /var/www/empty.local/project/html/web/ | grep "pma-" | tail -n 1`"
