bd6435d07e1ccb4b35fe5c2aa4e5be8127469172
faew <faew@ru5.net>
2018-09-25 06:36:28 +0000

    Move DocRoot

--- script/demo-yii2.sh
+++ script/demo-yii2.sh
@@ -7,15 +7,15 @@ echo "Usage for demo yii: sh demo-yii2.sh domain.name.without.www"
 else
  if [ -d /var/www/$1 ]
  then
- cd /var/www/$1/html/web
+ cd /var/www/$1/project/html/web
  wget https://github.com/yiisoft/yii2/releases/download/2.0.15/yii-basic-app-2.0.15.tgz
  tar -xvzf yii-basic-app-2.0.15.tgz
  mkdir vendor
- cp -R /var/www/$1/html/web/basic/vendor/yiisoft /var/www/$1/html/web/vendor
- cp -R /var/www/$1/html/web/basic/requirements.php /var/www/$1/html/web/requirements.php
- rm -f -R /var/www/$1/html/web/basic
- rm -f /var/www/$1/html/web/yii-basic-app-2.0.15.tgz
- chown -R $1:$1 /var/www/$1/html/web
+ cp -R /var/www/$1/project/html/web/basic/vendor/yiisoft /var/www/$1/project/html/web/vendor
+ cp -R /var/www/$1/project/html/web/basic/requirements.php /var/www/$1/project/html/web/requirements.php
+ rm -f -R /var/www/$1/project/html/web/basic
+ rm -f /var/www/$1/project/html/web/yii-basic-app-2.0.15.tgz
+ chown -R $1:$1 /var/www/$1/project/html/web
  else
  echo "Domain NOT $1 exists";
  fi
