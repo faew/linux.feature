7a3bc8b474b9f3004d0ad8620d27ceed5110d39c
faew <faew@ru5.net>
2018-10-19 09:24:55 +0400

    Only wget and yum connect to Internet

--- script/centos7/09-install-sphinx.sh
+++ script/centos7/09-install-sphinx.sh
@@ -3,7 +3,8 @@
 
 yum -y install postgresql-libs
 yum -y install unixODBC
-rpm -i http://sphinxsearch.com/files/sphinx-2.2.11-1.rhel7.x86_64.rpm
+wget -O /tmp/sphinx-2.2.11-1.rhel7.x86_64.rpm http://sphinxsearch.com/files/sphinx-2.2.11-1.rhel7.x86_64.rpm
+rpm -i /tmp/sphinx-2.2.11-1.rhel7.x86_64.rpm
 systemctl start searchd
 systemctl enable searchd
 echo "Configuration: /etc/sphinx/sphinx.conf"
