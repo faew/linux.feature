d9a3bdb3000833a91bdae620bc392049651aeb75
faew <faew@ru5.net>
2018-11-18 12:40:33 +0400

    Install php composer

---
+++ script/centos7/43-install-php-composer.sh
@@ -0,0 +1,7 @@
+# Copyright © 2018 Feature.su. All rights reserved.
+# Licensed under the Apache License, Version 2.0
+
+cd /usr/local/bin
+wget https://getcomposer.org/download/1.7.3/composer.phar
+chmod 755 composer.phar
+cd /tmp
