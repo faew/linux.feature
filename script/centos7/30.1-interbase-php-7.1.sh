61275a63b57165b058be199cd014ef0e01e73cf1
faew <faew@ru5.net>
2018-11-29 22:59:50 +0400

    Add compile php-interbase-7.1

---
+++ script/centos7/30.1-interbase-php-7.1.sh
@@ -0,0 +1,13 @@
+# Copyright © 2018 Feature.su. All rights reserved. 
+# Licensed under the Apache License, Version 2.0 
+
+# Warning: 44-install-firebird.sh install first
+
+cd /tmp/php-7.1.*/ext/interbase
+/usr/local/php71/bin/phpize
+./configure --with-php-config=/usr/local/php71/bin/php-config
+make
+make install
+echo 'extension=interbase.so' > /usr/local/php71/etc/conf.d/interbase.ini
+/usr/local/php71/bin/php -m | grep interbase
+systemctl reload httpd
