9f516cdad1cf41d6901441791604486a615e82f3
faew <faew@ru5.net>
2018-12-03 14:54:29 +0400

    Add pdo_firebird

--- script/centos7/30.1-interbase-php-7.1.sh
+++ script/centos7/30.1-interbase-php-7.1.sh
@@ -9,5 +9,13 @@ cd /tmp/php-7.1.*/ext/interbase
 make
 make install
 echo 'extension=interbase.so' > /usr/local/php71/etc/conf.d/interbase.ini
+
+cd /tmp/php-7.1.*/ext/pdo_firebird
+/usr/local/php71/bin/phpize
+./configure --with-php-config=/usr/local/php71/bin/php-config
+make
+make install
+echo 'extension=pdo_firebird.so' > /usr/local/php71/etc/conf.d/pdo_firebird.ini
+
 /usr/local/php71/bin/php -m | grep interbase
 systemctl reload httpd
