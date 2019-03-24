173436cd13492e772c42946b813d2fed147fbfaa
faew <faew@ru5.net>
2019-03-24 11:51:49 +0400

    Fix for new VHost configuration

--- script/centos7/05.1-install-apache-ssl.sh
+++ script/centos7/05.1-install-apache-ssl.sh
@@ -6,7 +6,7 @@ sed -i "s/Listen 443 https/Listen 0.0.0.0:443/g" /etc/httpd/conf.d/ssl.conf
 firewall-cmd --zone=public --permanent --add-service=https
 firewall-cmd --reload
 
-echo "Use VHostSSL empty.local" >> /etc/httpd/conf.modules.d/11-domains.conf
+echo "Use VHostSSL empty.local empty.local html/web" >> /etc/httpd/conf.modules.d/11-domains.conf
 cp /etc/pki/tls/certs/localhost.crt /etc/pki/tls/certs/empty.local.crt
 cp /etc/pki/tls/private/localhost.key /etc/pki/tls/private/empty.local.key
 cp /etc/pki/tls/certs/localhost.crt /etc/pki/tls/certs/empty.local-chain.crt
