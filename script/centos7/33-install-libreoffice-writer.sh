bffed01334515de7c559b912b64da99974b08a4c
faew <faew@ru5.net>
2019-04-02 17:34:51 +0400

    Update to 6.2

--- script/centos7/33-install-libreoffice-writer.sh
+++ script/centos7/33-install-libreoffice-writer.sh
@@ -4,20 +4,19 @@
 yum -y install java
 
 cd /tmp
-wget "http://download.documentfoundation.org/libreoffice/stable/5.3.7/rpm/x86_64/LibreOffice_5.3.7_Linux_x86-64_rpm.tar.gz"
-tar -xvzf ./LibreOffice_5.3.7_Linux_x86-64_rpm.tar.gz
-cd ./LibreOffice_5.3.7.2_Linux_x86-64_rpm/RPMS/
-rpm -i libreoffice5.3-ure-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-core-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-images-5.3.7.2-2.x86_64.rpm
-rpm -i libreoffice5.3-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-writer-5.3.7.2-2.x86_64.rpm
-rpm -i libreoffice5.3-writer-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-en-US-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-en-US-writer-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-ooofonts-5.3.7.2-2.x86_64.rpm
-rpm -i libobasis5.3-en-US-res-5.3.7.2-2.x86_64.rpm
+wget "http://download.documentfoundation.org/libreoffice/stable/6.2.2/rpm/x86_64/LibreOffice_6.2.2_Linux_x86-64_rpm.tar.gz"
+tar -xvzf ./LibreOffice_6.2.2_Linux_x86-64_rpm.tar.gz
+cd ./LibreOffice_6.2.2.2_Linux_x86-64_rpm/RPMS/
+rpm -i libreoffice6.2-ure-6.2.2.2-2.x86_64.rpm
+rpm -i libobasis6.2-core-6.2.2.2-2.x86_64.rpm
+rpm -i libobasis6.2-images-6.2.2.2-2.x86_64.rpm
+rpm -i libreoffice6.2-6.2.2.2-2.x86_64.rpm
+rpm -i libobasis6.2-writer-6.2.2.2-2.x86_64.rpm
+rpm -i libreoffice6.2-writer-6.2.2.2-2.x86_64.rpm
+rpm -i libobasis6.2-ooofonts-6.2.2.2-2.x86_64.rpm
+rpm -i libobasis6.2-en-US-6.2.2.2-2.x86_64.rpm
+rpm -i libreoffice6.2-en-US-6.2.2.2-2.x86_64.rpm
 
 # Simple test conversion
 # echo "<b>test-writer.html</b> <i>test-writer.html</i>" > /tmp/test.html
-# /opt/libreoffice5.3/program/soffice --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore  --writer --convert-to docx --outdir /tmp /tmp/test.html
+# /opt/libreoffice6.2/program/soffice --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --writer --convert-to docx --outdir /tmp /tmp/test.html
