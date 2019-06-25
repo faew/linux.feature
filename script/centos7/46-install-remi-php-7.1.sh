d31f62a2c0b9d365fcb9feaa1011395e2820c7ff
faew <faew@ru5.net>
2019-06-25 11:10:16 +0400

    Add OPcache

--- script/centos7/46-install-remi-php-7.1.sh
+++ script/centos7/46-install-remi-php-7.1.sh
@@ -12,7 +12,7 @@ yum-config-manager --enable remi-php71
 
 yum -y update php
 
-yum -y install php php-pdo php-mysql php-pear php-devel php-soap php-xml php-gd php-mcrypt php-dba php-mbstring php-pgsql php-posix php-intl php-pecl-memcache php-pecl-imagick
+yum -y install php php-opcache php-pdo php-mysql php-pear php-devel php-soap php-xml php-gd php-mcrypt php-dba php-mbstring php-pgsql php-posix php-intl php-pecl-memcache php-pecl-imagick
 
 sed -i "s/short_open_tag = Off/short_open_tag = On/g" /etc/php.ini
 sed -i "s/expose_php = On/expose_php = Off/g" /etc/php.ini
