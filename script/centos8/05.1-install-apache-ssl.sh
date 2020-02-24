73523b3ec918dd39dabba5abcf778c1c2f50ba32
faew <faew@ru5.net>
2020-02-24 15:48:47 +0400

    Add restart for generate keys

--- script/centos8/05.1-install-apache-ssl.sh
+++ script/centos8/05.1-install-apache-ssl.sh
@@ -6,6 +6,7 @@ sed -i "s/Listen 443 https/Listen 0.0.0.0:443/g" /etc/httpd/conf.d/ssl.conf
 firewall-cmd --zone=public --permanent --add-service=https
 firewall-cmd --reload
 
+systemctl restart httpd
 sleep 5
 
 echo "Use VHostSSL empty.local empty.local html/web" >> /etc/httpd/conf.modules.d/11-domains.conf
