b0df4b336327bda195275e882d061884836aa9fa
faew <faew@ru5.net>
2020-03-04 08:50:20 +0400

    Update to 6.3

--- script/centos7/33-install-libreoffice-writer.sh
+++ script/centos7/33-install-libreoffice-writer.sh
@@ -1,22 +1,22 @@
-# Copyright © 2017 Feature.su. All rights reserved.
+# Copyright © 2020 Feature.su. All rights reserved.
 # Licensed under the Apache License, Version 2.0
 
 yum -y install java
 
 cd /tmp
-wget "http://download.documentfoundation.org/libreoffice/stable/6.2.2/rpm/x86_64/LibreOffice_6.2.2_Linux_x86-64_rpm.tar.gz"
-tar -xvzf ./LibreOffice_6.2.2_Linux_x86-64_rpm.tar.gz
-cd ./LibreOffice_6.2.2.2_Linux_x86-64_rpm/RPMS/
-rpm -i libreoffice6.2-ure-6.2.2.2-2.x86_64.rpm
-rpm -i libobasis6.2-core-6.2.2.2-2.x86_64.rpm
-rpm -i libobasis6.2-images-6.2.2.2-2.x86_64.rpm
-rpm -i libreoffice6.2-6.2.2.2-2.x86_64.rpm
-rpm -i libobasis6.2-writer-6.2.2.2-2.x86_64.rpm
-rpm -i libreoffice6.2-writer-6.2.2.2-2.x86_64.rpm
-rpm -i libobasis6.2-ooofonts-6.2.2.2-2.x86_64.rpm
-rpm -i libobasis6.2-en-US-6.2.2.2-2.x86_64.rpm
-rpm -i libreoffice6.2-en-US-6.2.2.2-2.x86_64.rpm
+wget "http://download.documentfoundation.org/libreoffice/stable/6.3.5/rpm/x86_64/LibreOffice_6.3.5_Linux_x86-64_rpm.tar.gz"
+tar -xvzf ./LibreOffice_6.3.5_Linux_x86-64_rpm.tar.gz
+cd ./LibreOffice_6.3.5.2_Linux_x86-64_rpm/RPMS/
+rpm -i libreoffice6.3-ure-6.3.5.2-2.x86_64.rpm
+rpm -i libobasis6.3-ooofonts-6.3.5.2-2.x86_64.rpm
+rpm -i libobasis6.3-core-6.3.5.2-2.x86_64.rpm
+rpm -i libobasis6.3-images-6.3.5.2-2.x86_64.rpm
+rpm -i libreoffice6.3-6.3.5.2-2.x86_64.rpm
+rpm -i libobasis6.3-writer-6.3.5.2-2.x86_64.rpm
+rpm -i libreoffice6.3-writer-6.3.5.2-2.x86_64.rpm
+rpm -i libobasis6.3-en-US-6.3.5.2-2.x86_64.rpm
+
 
 # Simple test conversion
 # echo "<b>test-writer.html</b> <i>test-writer.html</i>" > /tmp/test.html
-# /opt/libreoffice6.2/program/soffice --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --writer --convert-to docx --outdir /tmp /tmp/test.html
+# /opt/libreoffice6.3/program/soffice --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --writer --convert-to docx --outdir /tmp /tmp/test.html
