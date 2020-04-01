6a10f3525f8d1bf7f0d3b9edc5ce7f83216a3d85
faew <faew@ru5.net>
2020-04-01 18:06:00 +0400

    Add nodejs v12

---
+++ script/centos8/52-install-nodejs12.sh
@@ -0,0 +1,6 @@
+# Copyright © 2020 Feature.su. All rights reserved.
+# Licensed under the Apache License, Version 2.0
+
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+yum install -y gcc-c++ make nodejs
+npm install -g inherits n
