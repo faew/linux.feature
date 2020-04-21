2dd813218ccea1806475735fb6f1565c0cce238b
faew <faew@ru5.net>
2020-04-21 12:41:02 +0400

    Update download link

--- script/centos7/30-compile-icu.sh
+++ script/centos7/30-compile-icu.sh
@@ -2,7 +2,7 @@
 # Licensed under the Apache License, Version 2.0 
 
 cd /tmp
-wget http://download.icu-project.org/files/icu4c/64.2/icu4c-64_2-src.tgz
+wget https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz
 tar -xvzf ./icu4c-64_2-src.tgz
 cd /tmp/icu/source
 ./configure
