b9494e68cd7aee2938685279fe5a831d3bac81fd
faew <faew@ru5.net>
2020-04-24 00:02:32 +0400

    Compile module imagick whith new ImageMagick lib.

---
+++ script/centos7/30.2-imagick-php-7.1.sh
@@ -0,0 +1,25 @@
+# Copyright © 2020 Feature.su. All rights reserved. 
+# Licensed under the Apache License, Version 2.0  
+
+# Warning: disable --with-imagick in 30-compile-php-7.1.sh!  There is compile with new ImageMagick
+
+cd /tmp
+wget https://imagemagick.org/download/ImageMagick-7.0.10-7.tar.gz
+tar -xvzf ./ImageMagick-7.0.10-7.tar.gz
+cd ImageMagick-7.0.10-7
+./configure
+make
+make install
+
+cd /tmp
+wget https://pecl.php.net/get/imagick-3.4.4.tgz
+tar -xvzf ./imagick-3.4.4.tgz
+cd imagick-3.4.4
+/usr/local/php71/bin/phpize
+./configure --with-imagick=/usr/local --with-php-config=/usr/local/php71/bin/php-config
+make
+make install
+
+echo 'extension=imagick.so' > /usr/local/php71/etc/conf.d/imagick.ini
+
+systemctl reload httpd
